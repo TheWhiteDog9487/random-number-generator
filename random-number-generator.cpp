a3e67beabfa55b739a24d3e9c847ea30262cd197
TheWhiteDog9487 <gaojian520one@outlook.com>
2021-12-04 13:26:11 +0800

    大提交

---
+++ random-number-generator.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    srand((unsigned int)time(NULL));
+    int input;
+    cout << "请输入随机数的最大值" << endl;
+    cout << "例：如果输入100，就会随机输出0-99" << endl;
+    cin >> input;
+    cout << "结果是：" << rand() % input << endl;
+    system("pause");}
